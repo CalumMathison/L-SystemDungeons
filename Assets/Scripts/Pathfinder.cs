a405cbecc834a6d648d41c40eec015ee39742559
Calum Mathison <40406464@live.napier.ac.uk>
2021-01-06 04:40:25 +0000

    Started adding A*

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Pathfinder
+{
+    class Location
+    {
+        public int X;
+        public int Y;
+        public int F;
+        public int G;
+        public int H;
+        public Location Parent;
+    }
+
+    static int ComputeHScore(int x, int y, int targetX, int targetY)
+    {
+        return Mathf.Abs(targetX - x) + Mathf.Abs(targetY - y);
+    }
+
+    public void Finder()
+    {
+        Location current = null;
+        Location start = new Location { X = 0, Y = 0 };
+        Location target = new Location { X = 1, Y = 1 };
+        List<Location> openList = new List<Location>();
+        List<Location> closedList = new List<Location>();
+        int g = 0;
+        openList.Add(start);
+
+
+        while (openList.Count > 0)
+        {
+            var lowest = openList.Min(l => l.F);
+            current = openList.First(l => l.F == lowest);
+
+            closedList.Add(current);
+
+            openList.Remove(current);
+
+
+        }
+    }
+}
